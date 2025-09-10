c576e19c891e5eaa69eed07f7c2af2e38c88cd94
Ahmet Karaca <ahmetkaraca.business@gmail.com>
2025-09-09 22:05:32 -0400

    Added meteors, fixed some bugs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import { ThemeToggle } from "@/components/ThemeToggle"
-import { StarBackground } from "./StarBackground"
+import { StarBackground } from "@/components/StarBackground"
 
 export const Home = () => {
-    return<div className = "min-h-screen bg-background overflow-x-hidden">
+    return(<div className = "min-h-screen bg-background text-foreground overflow-x-hidden">
         {/* Theme Toggle */}
             <ThemeToggle />
         {/* Background Effects */}
@@ -13,4 +13,5 @@ export const Home = () => {
 
         {/*Footer*/}
         </div>
-}
+    );
+};
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
 // https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
