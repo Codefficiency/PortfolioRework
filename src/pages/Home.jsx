7977aca9cc91dec9a4b7ff070f7e4f8c109c7f35
Ahmet Karaca <ahmetkaraca.business@gmail.com>
2025-09-09 21:28:53 -0400

    Added theme toggle switch, working on StarBackground + fixes
    Did minor fix to stop the infinite use state and made new components, ThemeToggle and StarBackground, whichchange the theme and act as the starry background of the website, respectively

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
+import { ThemeToggle } from "@/components/ThemeToggle"
+import { StarBackground } from "./StarBackground"
+
 export const Home = () => {
-    return<div className = "min-h-screen ">
-        Home
+    return<div className = "min-h-screen bg-background overflow-x-hidden">
         {/* Theme Toggle */}
-
+            <ThemeToggle />
         {/* Background Effects */}
-
+            <StarBackground />
         {/* Navbar */}
 
         {/* Main Content */}
