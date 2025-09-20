b7f90c33941ab238edd4c209b92ec9a6293de8da
Ahmet Karaca <ahmetkaraca.business@gmail.com>
2025-09-19 23:26:32 -0400

    Projects Section - Mac

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,4 +1,13 @@
-const projects = [{}];
+const projects = [
+    {
+        id: 1,
+        title: 'vexo.gg',
+        description:
+            'Vexo is a full-stack personal finance platform designed to act as an AI-powered financial adwisor for individuals.',
+        image: '/projects/vexo,gg',
+        tags: ['React Native', 'Firebase', 'JavaScript', 'OpenAI', 'Plaid'],
+    },
+];
 export const ProjectsSection = () => {
     return (
         <section>
